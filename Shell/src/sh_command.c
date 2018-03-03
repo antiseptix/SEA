08080a255752f950277b86012b9911428c5efdc1
Julien Groll <julien.groll@gmail.com>
2018-03-03 17:50:39 +0000

    Fixing execvp and added pipe

--- Shell/src/sh_command.c
+++ Shell/src/sh_command.c
@@ -10,6 +10,8 @@
 #include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
+#include <sys/wait.h>
+#include <errno.h>
 #include <sh_command.h>
 
 /**
@@ -32,16 +34,9 @@ void sh_exit(){
  * \param char ** args argument de la commande a executer (sous forme de tableau)
  */
 void sh_cd(char **args){
-	char path[40] = "";
-	printf("Args[1] %s", args[1]);
 	if(args[1] == NULL){
-		printf("Minimum un argument requis pour executer CD \n");
+		perror("Erreur");
 	}else{
-		//getcwd(path, sizeof(path));
-		//strcat(path, "/");
-		//strcat(path, args[1]);
-		//path = "/tmp";
-		printf("CHEMIN %s \n", path);
 		chdir(args[1]);
 	}
 }
@@ -51,7 +46,15 @@ void sh_cd(char **args){
  * \param char ** args argument de la commande a executer (sous forme de tableau)
  */
 void sh_echo(char **args){
-	printf("%s", args[1]);
+	int i = 1;
+	if(args[1] != NULL){
+		while(args[i] !=NULL){
+			printf("%s ", args[i]);
+			i++;
+		}
+		printf("\n");
+	}else
+		perror("Erreur");
 }
 
 /**
@@ -59,10 +62,9 @@ void sh_echo(char **args){
  */
 void sh_help(){
 	printf("Shell réalisé par Arnaud LAPENNA et Julien GROLL \n");
-	printf("Commande prise en charge \n");
-	// for (int i = 0; i < sizeof(command_str); i++) {
-	// 	printf("  %s\n", command_str[i]);
-	// }
+	printf("Commande prise en charge :\n");
+	printf("PWD | EXIT | CD | ECHO | HELP \n");
+	
 }
 
 /**
@@ -75,17 +77,23 @@ void execCommand(char **args){
 	//Obligé de comparer avec \n derrière
 	//Bonne solution ?? Don't know
 	
-	if (strcmp(args[0], "pwd\n") == 0){
+	if (strcmp(args[0], "pwd") == 0){
 	    sh_pwd();
-	}else if(strcmp(args[0], "exit\n") == 0){
+	}else if(strcmp(args[0], "exit") == 0){
 		sh_exit();
 	}else if (strcmp(args[0], "cd") == 0){
 	    sh_cd(args);
-	}else if(strcmp(args[0], "echo\n") == 0){
+	}else if(strcmp(args[0], "echo") == 0){
 		sh_echo(args);
-	}else if(strcmp(args[0], "help\n") == 0){
+	}else if(strcmp(args[0], "help") == 0){
 		sh_help();
 	}else{
-		system(args[0]);
+		int status_pid;
+		if(fork() == 0){
+        	execvp(args[0], args);
+        	perror("Erreur ");
+            exit(1);
+		}
+		wait(&status_pid);
 	}
 }
--- Shell/src/shell.c
+++ Shell/src/shell.c
@@ -5,12 +5,24 @@
  * \version 0.1
  * \date 23 fevrier 2018
  */
+ 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
 #include <sh_command.h>
+
+/**
+ * \brief Fonction permettant d'afficher le chemin courrant dans notre shell
+ */
+void displayPrompt(){
+	char path[40] = "";
+	//Récupère le chemin courrant
+	getcwd(path, sizeof(path));
+	//Print le chemin courrant en couleur
+	printf("\033[34m %s : \033[37m", path);
+}
 
 /**
  * \brief Fonction permettant de lancer le shell
@@ -21,17 +33,15 @@ int main(int argc, char *argv[]){
 		// Recherche du -b pour le lancer en mode commande simple du tpye : "./shell -b cd Document"
 		if(strcmp(argv[1], "-b") == 0){
 			// Il faudra surement changer le system et remplacer par le lancement de commande fait maison
-			execv
 		}
 	}else{		// Si le -b n'est pas trouvé on lance le shell
-		int c = 1;
 		char input[512] = "";
 	
 		//Ouverture du fichier en mode création (si il n'existe pas) , écriture seule et à la fin du fichier
 		int file = open("hystory.txt", O_CREAT | O_RDWR | O_APPEND);
 
 		//Boucle du shell
-		while (c==1) {
+		while (1) {
 			displayPrompt();
 			
 	
@@ -48,31 +58,22 @@ int main(int argc, char *argv[]){
 			for (i=0; i<10; i++){
 			    args[i] = malloc(40 * sizeof(char));
 			}
-	
+			//Permet de changer \n en \0 
+			input[strlen(input)-1] = '\0';
+			//Découpage de la chaine à chaque espace
 		    result = strtok(input, " ");
 		    i = 0;
 			while (result != NULL){
-			//Découpage de la chaine pour la mettre un tableau
+				//Copie dans la chaine dans un tableau d'argument
 				strcpy(args[i], result);
 				i++;
-				//printf("%s\n", result);
 				result = strtok( NULL, " ");
 			}
-			execCommand(args);
+			args[i] = NULL;
+			//execCommand(args);
 		}
 		//Fermeture du fichier
 		close(file);
 	}
 return 1;
-}	
-
-/**
- * \brief Fonction permettant d'afficher le chemin courrant dans notre shell
- */
-void displayPrompt(){
-	char path[40] = "";
-	//Récupère le chemin courrant
-	getcwd(path, sizeof(path));
-	//Print le chemin courrant en couleur
-	printf("\033[34m %s : \033[37m", path);
 }
